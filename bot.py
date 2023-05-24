6188bd537db93a81e13663c6c6f5c183036100f9
Richard Gao <richard.g2002@gmail.com>
2023-05-24 15:58:42 -0500

    Can run commands on servers now, needed to enable privileged intent: message_content

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import random
+
+from discord import app_commands
 from discord.ext import commands
 import responses
 
@@ -12,14 +14,22 @@ async def send_message(message, user_message):
 def run_discord_bot():
     TOKEN = 'MTExMDMzNjM5MTMzMTcyMTIyNg.GSLet1.X4C6hozQO9IQK4NZ0Amu4OB1JZhn7zqFoJJf24'
     intent = discord.Intents.default()
-    intent.messages = True
+    intent.message_content = True
+
     description = 'Example bot made by Richard Gao'
     bot = commands.Bot(command_prefix='?', description=description, intents = intent)
     @bot.event
     async def on_ready():
         print(f'{bot.user} is now running')
 
+        # note that auto syncing is bad (you get ratelimited to twice per minute
+        await bot.tree.sync(guild=discord.Object(id=847141350847545415))
+        # remove guild specification if you want to sync globally
+        print(f'Synced commands for {bot.user}.')
+
     @bot.command()
+    @app_commands.guilds(discord.Object(id = 847141350847545415))
+    # change ID to whatever server ID you want to sync to, leave out decorator if you want it to globally sync (will take around 1-24 hours to sync)
     async def poll(ctx, question: str, *args: str):
         '''
         Creates a poll on discord, with the first argument being the question asked, and the following arguments being the choices for the poll
